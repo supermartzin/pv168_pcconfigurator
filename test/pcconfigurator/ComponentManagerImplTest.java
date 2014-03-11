c0885e4d5cef38e0fa6b1c113dda2d3f2f789948
supermartzin <supermartzin@gmail.com>
2014-03-12 00:39:27 +0100

    Added Joda-Time and test methods in ComponentManager

--- test/pcconfigurator/ComponentManagerImplTest.java
+++ test/pcconfigurator/ComponentManagerImplTest.java
@@ -8,6 +8,7 @@ package pcconfigurator;
 
 import java.util.Set;
 import java.math.BigDecimal;
+import java.util.HashSet;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -67,6 +68,18 @@ public class ComponentManagerImplTest {
     @Test
     public void testGetComponentById() {
         Component component = new Component(10, "ASUS", new BigDecimal(89.90), ComponentTypes.MOTHERBOARD, 38, "Zakladna doska ASUS");
+        compManager.createComponent(component);
+        
+        Component result = compManager.getComponentById(component.getId());
+        
+        assertEquals("components do not match", component, result);
+        assertNotSame("components must not be the same objects", component, result);
+        
+        try
+        {
+            result = compManager.getComponentById(new Long(-5));
+            fail("ID cannot be negative number, exception must be thrown");
+        } catch(IllegalArgumentException ex) {}
         
     }
 
@@ -75,13 +88,25 @@ public class ComponentManagerImplTest {
      */
     @Test
     public void testFindAllComponents() {
-        System.out.println("findAllComponents");
-        ComponentManagerImpl instance = new ComponentManagerImpl();
-        Set<Component> expResult = null;
-        Set<Component> result = instance.findAllComponents();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Component comp1 = new Component(1, "AMD Graphics", new BigDecimal(149.80), ComponentTypes.GPU, 250, "R9 290X");
+        Component comp2 = new Component(2, "Creative", new BigDecimal(24.00), ComponentTypes.SOUNDCARD, 15, "SoundBlaster S150");
+        Component comp3 = new Component(3, "Kingston", new BigDecimal(37.50), ComponentTypes.RAM, 15, "DDR3 Memory 1600M");
+        Set<Component> comps = new HashSet<>();
+        comps.add(comp1);
+        comps.add(comp2);
+        comps.add(comp3);
+        
+        compManager.createComponent(comp1);
+        compManager.createComponent(comp2);
+        compManager.createComponent(comp3);
+        
+        Set<Component> result = compManager.findAllComponents();
+        
+        assertEquals("expected number of components does not match", comps.size(), result.size());
+        assertEquals("components do not match", comps, result);
+        assertNotSame("components must not be the same objects", comp1, compManager.getComponentById((long) 1));
+        assertNotSame("components must not be the same objects", comp2, compManager.getComponentById((long) 2));
+        assertNotSame("components must not be the same objects", comp3, compManager.getComponentById((long) 3));       
     }
 
     /**
