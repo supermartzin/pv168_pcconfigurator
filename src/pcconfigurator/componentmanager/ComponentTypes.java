5d53bded5033e766956c1d4881191478072675c8
supermartzin <supermartzin@gmail.com>
2014-04-24 16:09:03 +0200

    Edited ComponentTypes enumeration

--- src/pcconfigurator/componentmanager/ComponentTypes.java
+++ src/pcconfigurator/componentmanager/ComponentTypes.java
@@ -2,15 +2,23 @@ package pcconfigurator.componentmanager;
 
 public enum ComponentTypes {
 
-	GPU,
-	CPU,
-	MOTHERBOARD,
-	NETWORK_CARD,
-	HARD_DRIVE,
-	SOUNDCARD,
-	OPTICAL_DRIVE,
-	RAM,
-	CASE,
-	POWER_SUPPLY;
+	GPU("Grafická karta"),
+	CPU("Procesor"),
+	MOTHERBOARD("Základná doska"),
+	NETWORK_CARD("Sieťová karta"),
+	HARD_DRIVE("Pevný disk"),
+	SOUNDCARD("Zvuková karta"),
+	OPTICAL_DRIVE("Optická jednotka"),
+	RAM("Operačná pamäť"),
+	CASE("Skrinka"),
+	POWER_SUPPLY("Elektrický zdroj");
 
+        public String name;
+        ComponentTypes(String name){
+            this.name = name;
+        }
+        
+        String getName() {
+            return name;
+        }
 }
