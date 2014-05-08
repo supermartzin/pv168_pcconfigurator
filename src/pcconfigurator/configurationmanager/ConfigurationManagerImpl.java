97456206fc778728afa0b0c9a34458b4a791d9f5
supermartzin <supermartzin@gmail.com>
2014-05-08 12:18:58 +0200

    Changes of DateTime

--- src/pcconfigurator/configurationmanager/ConfigurationManagerImpl.java
+++ src/pcconfigurator/configurationmanager/ConfigurationManagerImpl.java
@@ -7,13 +7,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.Set;
+import java.util.TimeZone;
 import java.util.TreeSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.sql.DataSource;
+import org.apache.commons.dbcp.BasicDataSource;
 import pcconfigurator.exception.*;
 
 public class ConfigurationManagerImpl implements ConfigurationManager {
@@ -21,41 +27,47 @@ public class ConfigurationManagerImpl implements ConfigurationManager {
     public static final Logger LOGGER = Logger.getLogger(ConfigurationManagerImpl.class.getName());
     private final DataSource dataSource;
 
+    public ConfigurationManagerImpl() {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl("jdbc:derby://localhost:1527/pcconfiguration_test");
+        ds.setUsername("db");
+        ds.setPassword("db");
+        this.dataSource = ds;
+    }
+
     public ConfigurationManagerImpl(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
     @Override
     public void createConfiguration(Configuration configuration) {
-        if (this.dataSource == null) throw new IllegalStateException("DataSource is not set.");
-        
+        if (this.dataSource == null) {
+            throw new IllegalStateException("DataSource is not set.");
+        }
+
         testConfiguration(configuration);
         if (configuration.getId() != null) {
             throw new IllegalArgumentException("Configuration is already in database");
         }
-        
+
         PreparedStatement st = null;
         Connection conn = null;
         try {
             conn = dataSource.getConnection();
             conn.setAutoCommit(false);
             st = conn.prepareStatement("INSERT INTO database.configuration (name,creator,creation_time,last_update) "
-                                     + "VALUES (?,?,?,?)",
-                                        Statement.RETURN_GENERATED_KEYS);
+                    + "VALUES (?,?,?,?)",
+                    Statement.RETURN_GENERATED_KEYS);
             st.setString(1, configuration.getName());
             st.setString(2, configuration.getCreator());
-          
-            Date creationDate = new Date(configuration.getCreationTime().toEpochSecond(ZoneOffset.UTC) * 1000);
-            st.setDate(3, creationDate);
-
-            Date updateDate = new Date(configuration.getLastUpdate().toEpochSecond(ZoneOffset.UTC) * 1000);
-            st.setDate(4, updateDate);
+            st.setLong(3, configuration.getCreationTime().toEpochSecond(ZoneOffset.UTC));
+            st.setLong(4, configuration.getLastUpdate().toEpochSecond(ZoneOffset.UTC));
 
             int totalRows = st.executeUpdate();
             if (totalRows != 1) {
                 throw new InternalFailureException("Wrong number of rows!");
             }
-            ResultSet keys = st.getGeneratedKeys();           
+            ResultSet keys = st.getGeneratedKeys();
             configuration.setId(getKey(keys, configuration));
 
             conn.commit();
@@ -69,29 +81,33 @@ public class ConfigurationManagerImpl implements ConfigurationManager {
 
     @Override
     public Configuration getConfigurationById(Long id) {
-        if (this.dataSource == null) throw new IllegalStateException("DataSource is not set.");
-	if (id == null) throw new IllegalArgumentException("id is null");
-        
+        if (this.dataSource == null) {
+            throw new IllegalStateException("DataSource is not set.");
+        }
+        if (id == null) {
+            throw new IllegalArgumentException("id is null");
+        }
+
         Connection conn = null;
         PreparedStatement st = null;
         try {
             conn = dataSource.getConnection();
             st = conn.prepareStatement(
                     "SELECT conf_id,name,creator,creation_time,last_update "
-                  + "FROM database.configuration "
-                  + "WHERE conf_id = ?");
+                    + "FROM database.configuration "
+                    + "WHERE conf_id = ?");
             st.setLong(1, id);
-            
+
             ResultSet resultSet = st.executeQuery();
             if (resultSet.next()) {
                 Configuration configuration = new Configuration();
                 configuration.setId(resultSet.getLong("conf_id"));
                 configuration.setName(resultSet.getString("name"));
                 configuration.setCreator(resultSet.getString("creator"));
-                Instant instant = Instant.ofEpochMilli(resultSet.getDate("creation_time").getTime());
-                configuration.setCreationTime(LocalDateTime.ofInstant(instant, ZoneOffset.UTC));
-                Instant instant2 = Instant.ofEpochMilli(resultSet.getDate("last_update").getTime());
-                configuration.setLastUpdate(LocalDateTime.ofInstant(instant2, ZoneOffset.UTC));
+                LocalDateTime creationTime = LocalDateTime.ofEpochSecond(resultSet.getLong("creation_time"), 0, ZoneOffset.UTC);
+                configuration.setCreationTime(creationTime);
+                LocalDateTime lastUpdateTime = LocalDateTime.ofEpochSecond(resultSet.getLong("last_update"), 0, ZoneOffset.UTC);
+                configuration.setLastUpdate(lastUpdateTime);
 
                 if (resultSet.next()) {
                     throw new InternalFailureException("More configurations with same id found");
@@ -110,16 +126,18 @@ public class ConfigurationManagerImpl implements ConfigurationManager {
 
     @Override
     public Set<Configuration> findAllConfigurations() {
-        if (this.dataSource == null) throw new IllegalStateException("DataSource is not set.");
-        
+        if (this.dataSource == null) {
+            throw new IllegalStateException("DataSource is not set.");
+        }
+
         Set<Configuration> configSet = new TreeSet<>(Configuration.idComparator);
-        
+
         Connection conn = null;
         PreparedStatement st = null;
         try {
             conn = dataSource.getConnection();
             st = conn.prepareStatement("SELECT conf_id "
-                                     + "FROM database.configuration");
+                    + "FROM database.configuration");
             ResultSet resultSet = st.executeQuery();
             while (resultSet.next()) {
                 configSet.add(getConfigurationById((resultSet.getLong("conf_id"))));
@@ -129,23 +147,25 @@ public class ConfigurationManagerImpl implements ConfigurationManager {
         } finally {
             closeSources(conn, st);
         }
-        
+
         return configSet;
     }
 
     @Override
     public void updateConfiguration(Configuration configuration) {
-        if (this.dataSource == null) throw new IllegalStateException("DataSource is not set.");
+        if (this.dataSource == null) {
+            throw new IllegalStateException("DataSource is not set.");
+        }
         testConfiguration(configuration);
-        
+
         Connection conn = null;
         PreparedStatement st = null;
         try {
             conn = dataSource.getConnection();
             conn.setAutoCommit(false);
             st = conn.prepareStatement("UPDATE database.configuration "
-                                     + "SET name=?,creator=?,last_update=? "
-                                     + "WHERE conf_id=?");
+                    + "SET name=?,creator=?,last_update=? "
+                    + "WHERE conf_id=?");
             st.setString(1, configuration.getName());
             st.setString(2, configuration.getCreator());
             Date updateDate = new Date(configuration.getLastUpdate().toEpochSecond(ZoneOffset.UTC) * 1000);
@@ -165,17 +185,21 @@ public class ConfigurationManagerImpl implements ConfigurationManager {
 
     @Override
     public void deleteConfiguration(Configuration configuration) {
-        if (this.dataSource == null) throw new IllegalStateException("DataSource is not set.");
+        if (this.dataSource == null) {
+            throw new IllegalStateException("DataSource is not set.");
+        }
         testConfiguration(configuration);
-        if(configuration.getId()==null) throw new IllegalArgumentException("NO ID");
-        
+        if (configuration.getId() == null) {
+            throw new IllegalArgumentException("NO ID");
+        }
+
         Connection conn = null;
         PreparedStatement st = null;
         try {
             conn = dataSource.getConnection();
             conn.setAutoCommit(false);
             st = conn.prepareStatement("DELETE FROM database.configuration "
-                                     + "WHERE conf_id=?");
+                    + "WHERE conf_id=?");
             st.setLong(1, configuration.getId());
             if (st.executeUpdate() != 1) {
                 throw new InternalFailureException("Deleted more than 1 record");
@@ -191,22 +215,26 @@ public class ConfigurationManagerImpl implements ConfigurationManager {
 
     @Override
     public Set<Configuration> findConfigurationByName(String name) {
-        if (this.dataSource == null) throw new IllegalStateException("DataSource is not set.");
-        if (name == null) throw new IllegalArgumentException("name is null");
-        
+        if (this.dataSource == null) {
+            throw new IllegalStateException("DataSource is not set.");
+        }
+        if (name == null) {
+            throw new IllegalArgumentException("name is null");
+        }
+
         Set<Configuration> result = new TreeSet<>(Configuration.idComparator);
-        
+
         Connection conn = null;
         PreparedStatement st = null;
         try {
             conn = dataSource.getConnection();
             st = conn.prepareStatement("SELECT * "
-                                     + "FROM database.configuration "
-                                     + "WHERE name "
-                                     + "LIKE ?");
-            st.setString(1, "%"+name+"%");
+                    + "FROM database.configuration "
+                    + "WHERE name "
+                    + "LIKE ?");
+            st.setString(1, "%" + name + "%");
             ResultSet resultSet = st.executeQuery();
-            while(resultSet.next()){
+            while (resultSet.next()) {
                 Configuration configuration = new Configuration();
                 configuration.setId(resultSet.getLong("conf_id"));
                 configuration.setName(resultSet.getString("name"));
@@ -217,12 +245,12 @@ public class ConfigurationManagerImpl implements ConfigurationManager {
                 configuration.setLastUpdate(LocalDateTime.ofInstant(instant2, ZoneOffset.UTC));
                 result.add(configuration);
             }
-        } catch (SQLException ex){
-            LOGGER.log(Level.SEVERE,"Error during getting configurations from database: ",ex);
+        } catch (SQLException ex) {
+            LOGGER.log(Level.SEVERE, "Error during getting configurations from database: ", ex);
         } finally {
             closeSources(conn, st);
         }
-        
+
         return result;
     }
 
@@ -245,10 +273,10 @@ public class ConfigurationManagerImpl implements ConfigurationManager {
         if (configuration == null) {
             throw new IllegalArgumentException("Configuration argument is null");
         }
-        if (configuration.getCreator() == null){
+        if (configuration.getCreator() == null) {
             throw new IllegalArgumentException("Creator is null");
         }
-        if (configuration.getName() == null){
+        if (configuration.getName() == null) {
             throw new IllegalArgumentException("Name is null");
         }
         if (configuration.getCreator().isEmpty()) {
@@ -256,7 +284,7 @@ public class ConfigurationManagerImpl implements ConfigurationManager {
         }
         if (configuration.getName().isEmpty()) {
             throw new IllegalArgumentException("Configuration does not have a name.");
-        }        
+        }
         if (configuration.getCreationTime() == null) {
             throw new IllegalArgumentException("Time is null");
         }
@@ -264,20 +292,17 @@ public class ConfigurationManagerImpl implements ConfigurationManager {
             throw new IllegalArgumentException("Last update time is null");
         }
     }
-    
-    private void closeSources(Connection connection, Statement statement)
-    {
-        if (statement != null)
-        {
+
+    private void closeSources(Connection connection, Statement statement) {
+        if (statement != null) {
             try {
                 statement.close();
             } catch (SQLException ex) {
                 LOGGER.log(Level.SEVERE, "Closing of statement failed: ", ex);
             }
         }
-            
-        if (connection != null) 
-        {
+
+        if (connection != null) {
             try {
                 connection.setAutoCommit(true);
             } catch (SQLException ex) {
@@ -290,12 +315,13 @@ public class ConfigurationManagerImpl implements ConfigurationManager {
             }
         }
     }
-    
+
     private void rollbackChanges(Connection connection) {
-        if (connection != null)
-        {
+        if (connection != null) {
             try {
-                if (connection.getAutoCommit()) throw new IllegalStateException("Connection is in autocommit mode!");
+                if (connection.getAutoCommit()) {
+                    throw new IllegalStateException("Connection is in autocommit mode!");
+                }
                 connection.rollback();
             } catch (SQLException ex1) {
                 LOGGER.log(Level.SEVERE, "Rollback of update failed: ", ex1);
